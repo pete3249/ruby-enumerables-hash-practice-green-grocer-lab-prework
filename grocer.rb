dfa219a1a5e04d8b7f563ecb54b2d9c168fee7e6
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-10 01:44:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,7 +34,12 @@ end
 
 def apply_clearance(cart)
   cart.each do |item|
-    if 
+    if cart[item][:clearance]
+      clearance_price = cart[item][:price] * 0.8.round(2)
+      cart[item][:price] = clearance_price
+    end
+  end
+  puts cart
 end
 
 def checkout(cart, coupons)
