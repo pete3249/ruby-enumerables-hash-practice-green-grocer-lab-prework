c8d3e03d25299da693076e91e91739793ee68756
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-10 01:18:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,35 @@
-def consolidate_cart(cart)
-  # code here
-end
+     #{"AVOCADO"=>{:price=>3.0, :clearance=>true}}
 
-def apply_coupons(cart, coupons)
-  # code here
+def consolidate_cart(cart) #define method and arguments
+  hash = {} #create an empty hash
+  cart.each do |item| #iterating through each item in cart array 
+    if hash[item.keys[0]] #if item matches value "AVOCADO"
+      hash[item.keys[0]][:count] +=1 #add 1 to the :count
+    else #if item does not match first key
+      hash[item.keys[0]] = item[item.keys[0]] #the first key in the hash is equal to the item
+      hash[item.keys[0]][:count] = 1 #:count = 1
+    end #close if statement
+  end #close do...end
+  hash
+end #close method
+  
+def apply_coupons(cart, coupons) #define methods and arguments
+  coupons.each do |coupon| #iterating through each item in coupons array
+    if cart[coupon[:item]] #if item in cart matches coupon
+      item_coupon_name = coupon[:item] + " W/COUPON" #assign new name
+      if cart[item_coupon_name]
+        cart[item_coupon_name][:count] += coupon[:num]
+        cart[coupon[:item]][:count] =  cart[coupon[:item]][:count] - coupon[:num]
+      else
+        price_with_coupon = coupon[:cost]/coupon[:num]
+        cart[item_coupon_name] = {:price => price_with_coupon,
+                                  :count => coupon[:num],
+                                  :clearance => cart[coupon[:item]][:clearance]}
+        cart[coupon[:item]][:count] =  cart[coupon[:item]][:count] - coupon[:num]
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
